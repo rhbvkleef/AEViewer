bea7de5df686bae02338689742933908867e04ec
rhbvkleef <rhbvkleef@gmail.com>
2016-03-30 11:37:55 +0200

    Added test cases

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Helpers\AESystemJSONValidator as AEValidator;
+
 class TestCase extends Illuminate\Foundation\Testing\TestCase
 {
     /**
@@ -9,13 +11,22 @@ class TestCase extends Illuminate\Foundation\Testing\TestCase
      */
     protected $baseUrl = 'http://localhost';
 
+    public function testAESystemJSONValidator() {
+      $invalidMissingValueJSON = '[{"is_fluid":false,"size":1000,"is_craftable":false,"display_name":"Foobaritem","fingerprint":{"id":"9132","dmb":"0"}}]';
+      $invalidSyntaxJSON = '[{"is_fluid":false,"size":1000,"is_craftable":false,"display_name":"Foobaritem","fingerprint":{"id":"9132""dmg":"0"}}]';
+      $validJSON = '[{"is_fluid":false,"size":1000,"is_craftable":false,"display_name":"Foobaritem","fingerprint":{"id":"9132","dmg":"0"}}]';
+
+      $this->assertFalse(AEValidator::validateItemList($invalidMissingValueJSON));
+      $this->assertFalse(AEValidator::validateItemList($invalidSyntaxJSON));
+      $this->assertTrue(AEValidator::validateItemList($validJSON));
+    }
+
     /**
      * Creates the application.
      *
      * @return \Illuminate\Foundation\Application
      */
-    public function createApplication()
-    {
+    public function createApplication() {
         $app = require __DIR__.'/../bootstrap/app.php';
 
         $app->make(Illuminate\Contracts\Console\Kernel::class)->bootstrap();
